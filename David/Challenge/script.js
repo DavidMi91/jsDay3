82001eeda8499e8ea5339a42d6e1a2568c4468c2
David Michalitsch <david.michalitsch@gmail.com>
2022-01-31 14:57:42 +0100

    changes to challenge

--- David/Challenge/script.js
+++ David/Challenge/script.js
@@ -18,24 +18,26 @@
 //     }
 // }
 
-
 Student = ["John", "Jane"]
 MathGrades =[70,85]
 var studentName = prompt("Type a student's name please")
 
-// Student.find(
-//     function(str) {
-//         str == studentName;
-//         if(str=="John"{
-//             document.getElementById('student').innerHTML = Student[0];
-//             document.getElementById('points').innerHTML = MathGrades[0];
-
-//         }
-//     }
-// );
-Student.find(
-    function blabla (str){
-        return str ==  studentName;
+    if (Student.indexOf(studentName) > -1) { //indexof returns -1 for no match, and a number for match
+        document.getElementById("student").innerHTML = studentName;
+        var gradeIndex = Student.indexOf(studentName);
+        var points = MathGrades[gradeIndex];
+        document.getElementById("points").innerHTML = points;   
+      } else {
+        console.log("Sorry " + studentName + " you don't seem to be a math student."); //console log fail statement
+      }
+    if(points < 60){
+        document.getElementById("points").style.color = "red";
+    } else if (points > 60){
+        document.getElementById("points").style.color = "orange";
+    } else if (points > 70){
+        document.getElementById("points").style.color = "green";
+    } else if (points >= 100){
+        document.getElementById("points").style.color = "blue";
     }
-    );
-    
+      
+      
