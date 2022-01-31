d33031de050cb1bb905c6d946f4430a3babe84aa
David Michalitsch <david.michalitsch@gmail.com>
2022-01-31 13:34:31 +0100

    changed script.js

--- David/Intermediate/script.js
+++ David/Intermediate/script.js
@@ -44,8 +44,8 @@
 // Exercise 2 
 for (let i = 1; i < 101; i++ ) {
     if (i%5) {result = i};
-    if ((i%3)==0) { result = 'Fizz'};
-    if ((i%5)==0 && (!(i%3)==0)) { result = 'Buzz'};
-    if ((i%5)==0 && ((i%3)==0)) { result = 'FizzBuzz'};
+    if ((i%3)==0) { result = "Fizz"};
+    if ((i%5)==0 && (!(i%3)==0)) { result = "Buzz"};
+    if ((i%5)==0 && ((i%3)==0)) { result = "FizzBuzz"};
     console.log (result);
 }
