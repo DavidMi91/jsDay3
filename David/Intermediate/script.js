dd880f66a074556dae1f68dcd2ec064a742cdf2b
David Michalitsch <david.michalitsch@gmail.com>
2022-01-31 13:18:11 +0100

    aded basic exercises

---
+++ David/Intermediate/script.js
@@ -0,0 +1,44 @@
+// Ex1 
+
+// var martin = 76;
+// var thomas = 85;
+// var klaus = 65;
+// var maria = 93;
+// var david = 81;
+
+// function checkGrade(points){
+//     if(points < 60){
+//         console.log("F")
+//     }
+//     else if(points < 70){
+//         console.log("D")
+//     }
+//     else if(points < 80){
+//         console.log("C")
+//     }
+//     else if(points < 90){
+//         console.log("B")
+//     }
+//     else{
+//         console.log("A")
+//     }
+//     }
+
+//     console.log('Grade Martin:')
+//     checkGrade(martin)
+//     console.log('Grade Thomas:')
+//     checkGrade(thomas)
+
+// function calcAvg(param1,param2,param3,param4,param5){
+//     let avg = (param1+param2+param3+param4+param5)/5;
+//     console.log("Average points: " + avg)
+// }
+// var classAvg = calcAvg(martin,thomas,klaus,maria,david);
+
+
+// classAvg1 = (martin + thomas + klaus + maria + david)/5
+
+
+// checkGrade(classAvg1)
+
+// Exercise 2 
