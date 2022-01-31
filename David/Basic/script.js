74d3c65c7af78b2f7015a1d445c49f316471252b
David Michalitsch <david.michalitsch@gmail.com>
2022-01-31 10:26:55 +0100

    changes to script.js

---
+++ David/Basic/script.js
@@ -0,0 +1,12 @@
+// Exercise 1
+// var rndNumber =  Math.floor(Math.random() * (26 - -5) + -5);
+// console.log(rndNumber)
+
+// if(rndNumber <= 10){
+//     console.log("The weather is cold");
+// }
+// else{
+//     console.log("The weather is moderate")
+// }
+
+
